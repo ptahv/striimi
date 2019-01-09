8815cee5c0c5495fe6a1872d426c5fbf41db2131
Petri Tahvanainen <petri.tahvanainen@enersoft.fi>
2019-01-09 11:19:39 +0200

    Switched version control location

--- rollup.config.js
+++ rollup.config.js
@@ -1,55 +1,54 @@
 import nodeResolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
 import replace from 'rollup-plugin-replace'
 import { terser } from 'rollup-plugin-terser'
 
-export default [
-    {
-        input: 'src/index.js',
-        output: {
-          file: 'dist/striimi.js',
-          format: 'umd',
-          name: 'striimi',
-          indent: false
-        },
-        plugins: [
-          nodeResolve({
-            jsnext: true
-          }),
-          babel({
-            exclude: 'node_modules/**'
-          }),
-          replace({
-            'process.env.NODE_ENV': JSON.stringify('development')
-          })
-        ]
-    }, {
-        input: 'src/index.js',
-        output: {
-            file: 'dist/striimi.min.js',
-            format: 'umd',
-            name: 'striimi',
-            indent: false
-        },
-        plugins: [
-            nodeResolve({
-                jsnext: true
-            }),
-            babel({
-                exclude: 'node_modules/**'
-            }),
-            replace({
-                'process.env.NODE_ENV': JSON.stringify('production')
-            }),
-            terser({
-                compress: {
-                    pure_getters: true,
-                    unsafe: true,
-                    unsafe_comps: true,
-                    warnings: false
-                }
-            })
-        ]
-    }
+const plugins = [
+    nodeResolve({
+        jsnext: true
+    }),
+
+    babel({
+        exclude: 'node_modules/**'
+    }),
+
+    commonjs()
 ]
+
+export default [{
+    input: 'src/index.js',
+    output: {
+        file: 'dist/striimi.js',
+        format: 'umd',
+        name: 'striimi',
+        indent: false
+    },
+    plugins: plugins.concat([
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ])
+}, {
+    input: 'src/index.js',
+    output: {
+        file: 'dist/striimi.min.js',
+        format: 'umd',
+        name: 'striimi',
+        indent: false
+    },
+    plugins: plugins.concat([
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        terser({
+            compress: {
+                pure_getters: true,
+                unsafe: true,
+                unsafe_comps: true,
+                warnings: false
+            }
+        })
+    ])
+}]
     
--- src/index.js
+++ src/index.js
@@ -1,9 +1,104 @@
-/**
- * Copyright (c) 2015-present, Petri Tahvanainen.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+function emitBufferFactory() {
+    let emitBuffer = [];
+    let isEmitting = false;
 
-export { default } from './lib/striimi';
-export { default as stream } from './lib/striimi';
+    return ({listeners, values}) => {
+        emitBuffer.push({listeners, values});
+
+        if (isEmitting)
+            return;
+
+        isEmitting = true;
+        while (emitBuffer.length > 0) {
+            const {listeners, values} = emitBuffer[0];
+            listeners.map(fn => fn(...values));
+
+            emitBuffer.shift();
+        }
+
+        isEmitting = false;
+    }
+}
+
+export const striimi = (...initValues) => {
+	let storedValues = initValues;
+	let listeners = [];
+
+    const emitBuffer = emitBufferFactory();
+    
+	let stream = Object.assign(() => {
+        stream = null;
+        listeners = null;
+        storedValues = null;
+
+    }, (() => {
+        const _get = () => {
+            if (storedValues.length > 1)
+                return storedValues;
+
+            return storedValues[0];
+        }
+
+        const _set = (...values) => {
+            storedValues = values;
+            
+            return stream;
+        }
+
+        const _emit = (values) => {
+            storedValues = values;
+
+            if (listeners.length) {
+                emitBuffer({
+                    values,
+                    listeners,
+                });
+            }
+
+            return stream;
+        }
+
+        return {
+            subscribe(listener) {
+                listeners = listeners.concat(listener);
+
+                listener(...storedValues);
+
+                return () => {
+                    listeners = listeners.filter(l => l !== listener);
+                }
+            },
+
+            listen(listener) {
+                listeners = listeners.concat(listener);
+
+                return () => {
+                    listeners = listeners.filter(l => l !== listener);	
+                }
+            },
+
+            emit: (...values) => _emit(values),
+            
+            refresh: () => _emit(storedValues),
+            
+            reset: () => _emit(initValues),
+            
+            getValue: _get,
+
+            getValues: _get,
+
+            /**
+             * Undocumented, not intended for using. 
+             * Still here if one needs them.. 
+             */
+            
+            setValue: _set, 
+
+            setValues: _set
+        }
+    })());
+
+    return stream;
+}
+
+export default striimi;
